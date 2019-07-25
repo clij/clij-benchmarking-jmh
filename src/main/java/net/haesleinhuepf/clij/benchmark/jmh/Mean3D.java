83cf1be4edc6371d1c24b646515599e172850ae4
haesleinhuepf <rhaase@mpi-cbg.de>
2019-07-26 00:02:44 +0200

    benchmark single- versus multi-threaded processing for Mean3D

--- src/main/java/net/haesleinhuepf/clij/benchmark/jmh/Mean3D.java
+++ src/main/java/net/haesleinhuepf/clij/benchmark/jmh/Mean3D.java
@@ -3,6 +3,7 @@ package net.haesleinhuepf.clij.benchmark.jmh;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
+import ij.Prefs;
 import ij.plugin.Filters3D;
 import ij.plugin.filter.RankFilters;
 import ij.process.ByteProcessor;
@@ -25,6 +26,29 @@ public class Mean3D extends AbstractBenchmark {
         ImagePlus imp3D = images.getImp3Da();
         int rad = (int)radius.getRadiusF();
         IJ.run(imp3D, "Mean 3D...", "x=" + rad + " y=" + rad + " z=" + rad);
+        return imp3D;
+    }
+
+    @Benchmark
+    public Object ijrun_singlethreaded(Images images, Radius radius) {
+        int numThreads = Prefs.getThreads();
+        Prefs.setThreads(1);
+        ImagePlus imp3D = images.getImp3Da();
+        int rad = (int)radius.getRadiusF();
+        IJ.run(imp3D, "Mean 3D...", "x=" + rad + " y=" + rad + " z=" + rad);
+        Prefs.setThreads(numThreads);
+        return imp3D;
+    }
+
+
+    @Benchmark
+    public Object ijrun_multithreaded(Images images, Radius radius) {
+        int numThreads = Prefs.getThreads();
+        Prefs.setThreads(Runtime.getRuntime().availableProcessors());
+        ImagePlus imp3D = images.getImp3Da();
+        int rad = (int)radius.getRadiusF();
+        IJ.run(imp3D, "Mean 3D...", "x=" + rad + " y=" + rad + " z=" + rad);
+        Prefs.setThreads(numThreads);
         return imp3D;
     }
 
@@ -57,10 +81,40 @@ public class Mean3D extends AbstractBenchmark {
     }
 
     @Benchmark
-    public Object mcib3d(Images images, Radius radius) {
+    public Object ij_singlethreaded(Images images, Radius radius) {
+        int numThreads = Prefs.getThreads();
+        Prefs.setThreads(1);
+        ImagePlus imp3D = images.getImp3Da();
+        int rad = (int)radius.getRadiusF();
+        Filters3D.filter(imp3D.getStack(), Filters3D.MEAN, rad, rad, rad );
+        Prefs.setThreads(numThreads);
+        return imp3D;
+    }
+
+    @Benchmark
+    public Object ij_multithreaded(Images images, Radius radius) {
+        int numThreads = Prefs.getThreads();
+        Prefs.setThreads( Runtime.getRuntime().availableProcessors());
+        ImagePlus imp3D = images.getImp3Da();
+        int rad = (int)radius.getRadiusF();
+        Filters3D.filter(imp3D.getStack(), Filters3D.MEAN, rad, rad, rad );
+        Prefs.setThreads(numThreads);
+        return imp3D;
+    }
+
+    @Benchmark
+    public Object mcib3d_singlethreaded(Images images, Radius radius) {
         ImagePlus imp = images.getImp3Da();
         int rad = (int)radius.getRadiusF();
-        ImageStack res = FastFilters3D.filterImageStack(imp.getImageStack(), FastFilters3D.MEAN, rad, rad, rad, 0, false);
+        ImageStack res = FastFilters3D.filterImageStack(imp.getImageStack(), FastFilters3D.MEAN, rad, rad, rad, 1, false);
+        return new ImagePlus("filtered", res);
+    }
+
+    @Benchmark
+    public Object mcib3d_multithreaded(Images images, Radius radius) {
+        ImagePlus imp = images.getImp3Da();
+        int rad = (int)radius.getRadiusF();
+        ImageStack res = FastFilters3D.filterImageStack(imp.getImageStack(), FastFilters3D.MEAN, rad, rad, rad, Runtime.getRuntime().availableProcessors(), false);
         return new ImagePlus("filtered", res);
     }
 }
