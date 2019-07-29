c7a0a07aaade6513e5e8efc0e855569b94dfb3f4
haesleinhuepf <rhaase@mpi-cbg.de>
2019-07-29 17:02:18 +0200

    benchmark execution in batches because jmh doesn't save intermediate results

--- bench.bat
+++ bench.bat
@@ -1,1 +1,57 @@
-java -jar target/benchmarks3.jar -rf csv
+java -jar target/benchmarks3.jar AddImagesWeighted2D -rf csv
+copy jmh-result.csv jmh-result_myers22_AddImagesWeighted2D.csv
+java -jar target/benchmarks3.jar AddImagesWeighted3D -rf csv
+copy jmh-result.csv jmh-result_myers22_AddImagesWeighted3D.csv
+java -jar target/benchmarks3.jar AddScalar2D-rf csv
+copy jmh-result.csv jmh-result_myers22_AddScalar2D.csv
+java -jar target/benchmarks3.jar AddScalar3D-rf csv
+copy jmh-result.csv jmh-result_myers22_AddScalar3D.csv
+java -jar target/benchmarks3.jar AutoThreshold2D-rf csv
+copy jmh-result.csv jmh-result_myers22_AutoThreshold2D.csv
+java -jar target/benchmarks3.jar AutoThreshold3D -rf csv
+copy jmh-result.csv jmh-result_myers22_AutoThreshold3D.csv
+java -jar target/benchmarks3.jar BinaryAnd2D -rf csv
+copy jmh-result.csv jmh-result_myers22_BinaryAnd2D.csv
+java -jar target/benchmarks3.jar BinaryAnd3D -rf csv
+copy jmh-result.csv jmh-result_myers22_BinaryAnd3D.csv
+java -jar target/benchmarks3.jar Erode2D -rf csv
+copy jmh-result.csv jmh-result_myers22_Erode2D.csv
+java -jar target/benchmarks3.jar Erode3D -rf csv
+copy jmh-result.csv jmh-result_myers22_Erode3D.csv
+java -jar target/benchmarks3.jar Flip2D -rf csv
+copy jmh-result.csv jmh-result_myers22_Flip2D.csv
+java -jar target/benchmarks3.jar Flip3D -rf csv
+copy jmh-result.csv jmh-result_myers22_Flip3D.csv
+java -jar target/benchmarks3.jar GaussianBlur2D -rf csv
+copy jmh-result.csv jmh-result_myers22_GaussianBlur2D.csv
+java -jar target/benchmarks3.jar GaussianBlur3D -rf csv
+copy jmh-result.csv jmh-result_myers22_GaussianBlur3D.csv
+java -jar target/benchmarks3.jar MaximumZProjection -rf csv
+copy jmh-result.csv jmh-result_myers22_MaximumZProjection.csv
+java -jar target/benchmarks3.jar MultiplyScalar2D -rf csv
+copy jmh-result.csv jmh-result_myers22_MultiplyScalar2D.csv
+java -jar target/benchmarks3.jar MultiplyScalar3D -rf csv
+copy jmh-result.csv jmh-result_myers22_MultiplyScalar3D.csv
+java -jar target/benchmarks3.jar RadialReslice -rf csv
+copy jmh-result.csv jmh-result_myers22_RadialReslice.csv
+java -jar target/benchmarks3.jar Rotate2D -rf csv
+copy jmh-result.csv jmh-result_myers22_Rotate2D.csv
+java -jar target/benchmarks3.jar Rotate3D -rf csv
+copy jmh-result.csv jmh-result_myers22_Rotate3D.csv
+java -jar target/benchmarks3.jar Threshold2D -rf csv
+copy jmh-result.csv jmh-result_myers22_Threshold2D.csv
+java -jar target/benchmarks3.jar Threshold3D -rf csv
+copy jmh-result.csv jmh-result_myers22_Threshold3D.csv
+java -jar target/benchmarks3.jar Mean2D -rf csv
+copy jmh-result.csv jmh-result_myers22_Mean2D.csv
+java -jar target/benchmarks3.jar Mean3D -rf csv
+copy jmh-result.csv jmh-result_myers22_Mean3D.csv
+java -jar target/benchmarks3.jar Median2D -rf csv
+copy jmh-result.csv jmh-result_myers22_Median2D.csv
+java -jar target/benchmarks3.jar Median3D -rf csv
+copy jmh-result.csv jmh-result_myers22_Median3D.csv
+java -jar target/benchmarks3.jar Minimum2D -rf csv
+copy jmh-result.csv jmh-result_myers22_Minimum2D.csv
+java -jar target/benchmarks3.jar Minimum3D -rf csv
+copy jmh-result.csv jmh-result_myers22_Minimum3D.csv
+
