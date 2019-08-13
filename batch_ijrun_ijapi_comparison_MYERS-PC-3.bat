f4edd271a40932632c643573a08910eec72c6f11
haesleinhuepf <rhaase@mpi-cbg.de>
2019-08-13 10:54:36 +0200

    update ijapi-ijrun script with recent method calls

--- batch_ijrun_ijapi_comparison_MYERS-PC-3.bat
+++ batch_ijrun_ijapi_comparison_MYERS-PC-3.bat
@@ -1,56 +1,56 @@
-java -jar target\benchmarks3.jar .AddScalar2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .AddScalar2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.AddScalar2D.ijrun.csv
-java -jar target\benchmarks3.jar .AddScalar2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .AddScalar2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.AddScalar2D.ijapi.csv
-java -jar target\benchmarks3.jar .AddScalar3D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .AddScalar3D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.AddScalar3D.ijrun.csv
-java -jar target\benchmarks3.jar .AddScalar3D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .AddScalar3D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.AddScalar3D.ijapi.csv
-java -jar target\benchmarks3.jar .AutoThreshold2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .AutoThreshold2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.AutoThreshold2D.ijrun.csv
-java -jar target\benchmarks3.jar .AutoThreshold2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .AutoThreshold2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.AutoThreshold2D.ijapi.csv
-java -jar target\benchmarks3.jar .Erode2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Erode2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Erode2D.ijrun.csv
-java -jar target\benchmarks3.jar .Erode2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Erode2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Erode2D.ijapi.csv
-java -jar target\benchmarks3.jar .Flip2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Flip2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Flip2D.ijrun.csv
-java -jar target\benchmarks3.jar .Flip2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Flip2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Flip2D.ijapi.csv
-java -jar target\benchmarks3.jar .GaussianBlur2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .GaussianBlur2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.GaussianBlur2D.ijrun.csv
-java -jar target\benchmarks3.jar .GaussianBlur2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .GaussianBlur2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.GaussianBlur2D.ijapi.csv
-java -jar target\benchmarks3.jar .GaussianBlur3D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .GaussianBlur3D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.GaussianBlur3D.ijrun.csv
-java -jar target\benchmarks3.jar .GaussianBlur3D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .GaussianBlur3D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.GaussianBlur3D.ijapi.csv
-java -jar target\benchmarks3.jar .MaximumZProjection.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .MaximumZProjection.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.MaximumZProjection.ijrun.csv
-java -jar target\benchmarks3.jar .MaximumZProjection.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .MaximumZProjection.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.MaximumZProjection.ijapi.csv
-java -jar target\benchmarks3.jar .Mean2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Mean2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Mean2D.ijrun.csv
-java -jar target\benchmarks3.jar .Mean2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Mean2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Mean2D.ijapi.csv
-java -jar target\benchmarks3.jar .Mean3D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Mean3D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Mean3D.ijrun.csv
-java -jar target\benchmarks3.jar .Mean3D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Mean3D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Mean3D.ijapi.csv
-java -jar target\benchmarks3.jar .Minimum3D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Minimum3D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Minimum3D.ijrun.csv
-java -jar target\benchmarks3.jar .Minimum3D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .Minimum3D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.Minimum3D.ijapi.csv
-java -jar target\benchmarks3.jar .MultiplyScalar2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .MultiplyScalar2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.MultiplyScalar2D.ijrun.csv
-java -jar target\benchmarks3.jar .MultiplyScalar2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .MultiplyScalar2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.MultiplyScalar2D.ijapi.csv
-java -jar target\benchmarks3.jar .MultiplyScalar3D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .MultiplyScalar3D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.MultiplyScalar3D.ijrun.csv
-java -jar target\benchmarks3.jar .MultiplyScalar3D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .MultiplyScalar3D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.MultiplyScalar3D.ijapi.csv
-java -jar target\benchmarks3.jar .FixedThreshold2D.ijrun  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .FixedThreshold2D.ijrun$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.FixedThreshold2D.ijrun.csv
-java -jar target\benchmarks3.jar .FixedThreshold2D.ijapi  -p size=1 -p radius=2  -rf csv
+java -jar target\benchmarks3.jar .FixedThreshold2D.ijapi$  -p size=1 -p radius=2  -rf csv
 copy jmh-result.csv C:\structure\code\clij-benchmarking-revision\clij_benchmarking_jmh\ijrun_ijapi_comparison\jmh-result_MYERS-PC-3_.FixedThreshold2D.ijapi.csv
