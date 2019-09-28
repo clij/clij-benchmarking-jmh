3edb43537e27fede56fb35030543cf53ff777a06
haesleinhuepf <rhaase@mpi-cbg.de>
2019-09-28 12:29:42 +0200

    removed non-existing 15th image size in index

--- src/main/java/net/haesleinhuepf/clij/benchmark/jmh/AbstractBenchmark.java
+++ src/main/java/net/haesleinhuepf/clij/benchmark/jmh/AbstractBenchmark.java
@@ -53,7 +53,7 @@ public class AbstractBenchmark {
     public static class Images {
         // Use a single pixel for testing the clijNoOp
         //@Param({"1"})
-        @Param({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"})
+        @Param({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"})
         //@Param({"4096"/*, "2048"*/})
         int size;
 
