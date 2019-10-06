439660b3c1c32651fccdc0a190a6d610f062c567
haesleinhuepf <rhaase@mpi-cbg.de>
2019-10-06 11:31:56 +0200

    added batch for remaining benchmarks

---
+++ batch_remaining_comparison_MYERS-PC-21.bat
@@ -0,0 +1,78 @@
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur2D.ijrun$  -p size=11 -p radius=28  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur2D_radius_28.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur2D.clij$  -p size=11 -p radius=28  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur2D_radius_28.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur3D.ijrun$  -p size=7 -p radius=28  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur3D_radius_28.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur3D.clij$  -p size=7 -p radius=28  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur3D_radius_28.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum2D.ijrun$  -p size=11 -p radius=14  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.Minimum2D_radius_14.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum2D.clij$  -p size=11 -p radius=14  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.Minimum2D_radius_14.clij.csv
+
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur2D.ijrun$  -p size=11 -p radius=30  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur2D_radius_30.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur2D.clij$  -p size=11 -p radius=30  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur2D_radius_30.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur3D.ijrun$  -p size=7 -p radius=30  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur3D_radius_30.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .GaussianBlur3D.clij$  -p size=7 -p radius=30  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.GaussianBlur3D_radius_30.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum2D.ijrun$  -p size=11 -p radius=15  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.Minimum2D_radius_15.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum2D.clij$  -p size=11 -p radius=15  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\kernelsize\jmh-result_MYERS-PC-21_.Minimum2D_radius_15.clij.csv
+
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Mean2D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Mean2D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Mean2D.clij_sphere$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Mean2D.clij_sphere.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Mean3D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Mean3D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Mean3D.clij_sphere$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Mean3D.clij_sphere.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Median2D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Median2D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Median2D.clij_sphere$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Median2D.clij_sphere.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Median3D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Median3D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Median3D.clij_sphere$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Median3D.clij_sphere.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum2D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Minimum2D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum2D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Minimum2D.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum3D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Minimum3D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Minimum3D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Minimum3D.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .MultiplyScalar2D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.MultiplyScalar2D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .MultiplyScalar2D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.MultiplyScalar2D.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .MultiplyScalar3D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.MultiplyScalar3D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .MultiplyScalar3D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.MultiplyScalar3D.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .RadialReslice.ijapi$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.RadialReslice.ijapi.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .RadialReslice.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.RadialReslice.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Rotate2D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Rotate2D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Rotate2D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Rotate2D.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Rotate3D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Rotate3D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .Rotate3D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.Rotate3D.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .FixedThreshold2D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.FixedThreshold2D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .FixedThreshold2D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.FixedThreshold2D.clij.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .FixedThreshold3D.ijrun$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.FixedThreshold3D.ijrun.csv
+java -Xms12G -Xmx12G  -jar target\benchmarks3.jar .FixedThreshold3D.clij$  -p radius=2  -rf csv
+copy jmh-result.csv C:\structure\code\clij-benchmarking\data\benchmarking-jmh\imagesize\jmh-result_MYERS-PC-21_.FixedThreshold3D.clij.csv
